fb9d773c96eff77d31ed8228faa5d479ccfad8bc
liletian <liletian.us@gmail.com>
2020-10-20 17:24:02 -0700

    Update uart.py

--- uart.py
+++ uart.py
@@ -10,3 +10,24 @@ import serial
 
 ser = serial.Serial('COM4', 115200, timeout=1)
 
+ser.write(str.encode('w0103'))
+ser.write(str.encode('w027c'))
+ser.write(str.encode('w0080'))
+
+ser.write(str.encode('r'))
+ser.write(str.encode('06'))
+a=ser.read(2)
+
+
+
+
+
+
+while True:
+    for line in ser.read():
+
+        print(str(count) + str(': ') + chr(line) )
+        count = count+1
+
+ser.close()
+
