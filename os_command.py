f8460deb5cb88c1e26ec098d48a8d5958bdb94ab
liletian <liletian.us@gmail.com>
2020-10-25 20:43:30 -0700

    Create os_command.py

---
+++ os_command.py
@@ -0,0 +1,20 @@
+import os
+os.chdir(path)
+print("Current Working Directory " , os.getcwd())
+os.chdir("/home/varun/temp")
+
+
+try:
+    # Change the current working Directory    
+    os.chdir("/home/varun/temp")
+    print("Directory changed")
+except OSError:
+    print("Can't change the Current Working Directory") 
+    
+    
+# Check if New path exists
+if os.path.exists("/home/varun/temp") :
+    # Change the current working Directory    
+    os.chdir("/home/varun/temp")
+else:
+    print("Can't change the Current Working Directory")  
