8048a7d8949fb3c3f0d3594b7a89f97cf55bb2da
liletian <liletian.us@gmail.com>
2021-04-28 18:03:33 -0700

    Create ztransform.py

---
+++ ztransform.py
@@ -0,0 +1,33 @@
+import numpy as np
+from plot_zplane import zplane 
+a = np.array([1.0])
+b = np.array([1.9,-1.9])
+zplane(b,a)
+
+w, H = signal.freqz(b,a)
+
+
+import matplotlib.pyplot as plt
+plt.subplot(311)
+plt.plot(w, 20 * np.log10(np.abs(H)), 'b')
+plt.ylabel('Amplitude [dB]', color='b')
+plt.xlabel('Frequency [rad/sample]')
+plt.grid(True)
+plt.axis('tight')
+plt.subplot(312)
+angles = np.unwrap(np.angle(H))
+plt.plot(w, angles, 'g')
+plt.ylabel('Angle (radians)', color='g')
+plt.xlabel('Frequency [rad/sample]')
+plt.grid(True)
+plt.axis('tight')
+
+plt.subplot(313)
+grd = np.diff(angles)/np.diff(w)
+grd_temp=np.append(grd,0)
+plt.plot(w, grd_temp, 'r')
+plt.ylabel('grd', color='g')
+plt.xlabel('Frequency [rad/sample]')
+plt.grid(True)
+plt.axis('tight')
+plt.show()
